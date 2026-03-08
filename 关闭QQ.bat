0d086f97cb7f1b42de64f3770df6fd0e8978e28f
qingxibb <2045791529@qq.com>
2026-03-09 01:55:02 +0800

    chore: remove unused QQ scripts

--- 关闭QQ.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo 欢迎使用獨領風騷电台.
-taskkill /f /im QQMusic.exe
-ping /n 2 127.0.0.1 >nul
-echo 关闭!
--- 关闭QQ.vbs
+++
@@ -1,2 +0,0 @@
-set ws=wscript.createobject("wscript.shell")
-ws.run "¹Ø±ÕQQ.bat /start",0
